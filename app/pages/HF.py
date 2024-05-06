597785b19fc26919cd86e885daaabc4cadb8467e
chriztopherton <christopher.ton@sjsu.edu>
2024-05-05 18:59:11 -0700

    add pc RAG, secrets keys

--- app/pages/HF.py
+++ app/pages/HF.py
@@ -8,8 +8,15 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings 
+from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain_pinecone import PineconeVectorStore
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+
 import re
 import requests
 from typing import Literal
@@ -31,6 +38,9 @@ if 'model_base_url' not in st.session_state: st.session_state['model_base_url'] 
 
 hf_token = os.getenv("hf_token")
 
+# pinecone = Pinecone()
+
+
 
 # db = FAISS.load_local("./funcheap_2024-04-26_2024-06-25_db",
 #                       HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'}),
@@ -40,24 +50,37 @@ hf_token = os.getenv("hf_token")
 
 with st.sidebar:
     with st.expander("HF token"):
-        user_hf_token = st.text_input("","",key="hf_key")
-        st.session_state['user_hf_token'] = user_hf_token
+        # user_hf_token = st.text_input("","",key="hf_key")
+        st.session_state['user_hf_token'] = st.secrets['hf_token']
         
     with st.expander("LLM Inference Endpoint"):
-        model_base_url = st.text_input("","",key="model_key")
-        st.session_state['model_base_url'] = model_base_url
+        # model_base_url = st.text_input("","",key="model_key")
+        st.session_state['model_base_url'] = st.secrets['model_base_url']
         
-headers = {"Accept" : "application/json","Authorization": f"Bearer {user_hf_token}","Content-Type": "application/json" }
-
 
 system_prompt = '''Answer the question as if you are a travel agent and your goal is to provide excellent customer service and to provide
         personalized travel recommendations with reasonings based on their question. Do not repeat yourself or include any links or HTML.'''
+        
+index_name = 'wanderchat-travel-advisory-rag'
 
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+embed_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+vectorstore = Pinecone.from_existing_index(
+    index_name, embed_model.embed_query)
+
+headers = {"Accept" : "application/json","Authorization": f"Bearer {st.secrets['hf_token']}","Content-Type": "application/json" }
 def query(payload):
-  response = requests.post(model_base_url, headers=headers, json=payload)
+  response = requests.post(st.secrets['model_base_url'], headers=headers, json=payload)
   return response.json()
+
+def find_match(input):
+    # input_em = embeddings.encode(input).tolist()
+    result = vectorstore.similarity_search(input)
+    # return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+    return ' '.join([d.page_content for d in result])
     
-if user_hf_token and model_base_url:
+if st.secrets['hf_token'] and st.secrets['model_base_url']:
     
     chat = ChatOpenAI(model_name="tgi",openai_api_key=st.session_state['user_hf_token'],openai_api_base=st.session_state['model_base_url'],)
     client = OpenAI(base_url=st.session_state['model_base_url'], api_key=st.session_state['user_hf_token'])
@@ -89,35 +112,47 @@ if user_hf_token and model_base_url:
                         time.sleep(1)
                         # responce=convo.predict(input=prompt)
                         
-                        # prompt = f"""{system_prompt}
-                        #     Question: {prompt}"""
-                        # input_len = len(prompt.split())
-                        # max_token_len = 1500-input_len-100 #100 buffer
+                        context = find_match(prompt)
+                        
+                        prompt = f"""{system_prompt}
+                            Context: {context} \n\n
+                            Question: {prompt}"""
+                        
+                        #st.sidebar.write(prompt)
 
-                        # start_time = time.time()
-                        # while True: #while loop for token
-                        #     answer = query({'inputs': f"<s>[INST] {prompt} [/INST]",
-                        #                 'parameters': {"max_new_tokens": max_token_len}})
-                        #     if 'error' not in answer:
-                        #         break  #exit the while loop if there is no error
-                        #     max_token_len -= 100 #reduce by 100 in while loop
-                        #     print(f"Failed to process prompt with token length: {max_token_len}")
-                        #     if max_token_len <= 0:
-                        #         break
-                        # end_time = time.time()
-                        # duration = end_time - start_time
+                            
+                        input_len = len(prompt.split())
+                        max_token_len = 1500-input_len-100 #100 buffer
+
+                        start_time = time.time()
+                        while True: #while loop for token
+                            answer = query({'inputs': f"<s>[INST] {prompt} [/INST]",
+                                        'parameters': {"max_new_tokens": max_token_len}})
+                            if 'error' not in answer:
+                                break  #exit the while loop if there is no error
+                            max_token_len -= 100 #reduce by 100 in while loop
+                            print(f"Failed to process prompt with token length: {max_token_len}")
+                            if max_token_len <= 0:
+                                break
+                        end_time = time.time()
+                        duration = end_time - start_time
                         
-                        # answer = answer[0]['generated_text'].replace(f"<s>[INST] {prompt} [/INST]","")
-                        # answer = answer.replace(" . ",". ").strip()
-                        # answer = re.sub(r'<ANSWER>.*$', '', answer, flags=re.DOTALL) #RAFT specific
-                        # responce = re.sub(r'Final answer: .*$', '', answer, flags=re.DOTALL) #RAFT specific
+                        print(answer)
                         
-                        chat_completion = client.chat.completions.create(
-                            model="tgi",messages=[{"role": "user","content": prompt}],stream=False,max_tokens=1000)
-                        responce = chat_completion.choices[0].message.content
-                        responce = responce.replace(" . ",". ").strip()
-                        responce = re.sub(r'<ANSWER>.*$', '', responce, flags=re.DOTALL) #RAFT specific
-                        responce = re.sub(r'Final answer: .*$', '', responce, flags=re.DOTALL) #RAFT specific
+                        answer = answer[0]['generated_text'].replace(f"<s>[INST] {prompt} [/INST]","")
+                        answer = answer.replace(" . ",". ").strip()
+                        answer = re.sub(r'<ANSWER>.*$', '', answer, flags=re.DOTALL) #RAFT specific
+                        responce = re.sub(r'Final answer: .*$', '', answer, flags=re.DOTALL) #RAFT specific
+                        
+
+                                                # chat_completion = client.chat.completions.create(
+                        #     model="tgi",messages=[{"role": "user","content": f"Context:\n {context} \n\n Query:\n{prompt}"}],stream=False,max_tokens=1350)
+                        #     # model="tgi",messages=[{"role": "user","content": prompt}],stream=False,max_tokens=1300)
+                        # responce = chat_completion.choices[0].message.content
+                        
+                        # responce = responce.replace(" . ",". ").strip()
+                        # responce = re.sub(r'<ANSWER>.*$', '', responce, flags=re.DOTALL) #RAFT specific
+                        # responce = re.sub(r'Final answer: .*$', '', responce, flags=re.DOTALL) #RAFT specific
                         
                         st.write(responce)
                         
